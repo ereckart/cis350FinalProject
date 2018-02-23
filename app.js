625c5a28a278f460cc76d9013e362c8153e8dab9
Daniella Erulker <derulker@gmail.com>
2018-02-22 23:48:57 -0500

    fixed path

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var express = require("express");
 var app = express();
+var path = require('path');
 
 app.engine('html', require('ejs').__express);
 app.set('view engine', 'html');
-app.set('views', './public');
-app.use(express.static(__dirname + 'public'));
+app.set('views', path.join(__dirname,'public'));
+app.use(express.static(path.join(__dirname,'public')));
 
 app.get('/', (req, res) => res.render('login'));
 
