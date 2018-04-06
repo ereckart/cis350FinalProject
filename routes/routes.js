950c4865f97267422353c0a82f4ca3803603cc8e
ereckart <ereckart@seas.upenn.edu>
2018-04-06 11:12:37 -0400

    Update description works, still need to display it though

--- routes/routes.js
+++ routes/routes.js
@@ -138,7 +138,7 @@ var newClub = function(req, res) {
     });
 }
 
-//DESCRIPTION OF FUNCTION
+//page display join option
 var joinClubPage = function(req, res) {
 	console.log('inside join club page');
 	req.session.clubToJoin = req.params.clubname;
@@ -150,7 +150,7 @@ var joinClubPage = function(req, res) {
 	}
 }
 
-//DESCRIPTION OF FUNCTION
+//user can join club
 var joinClub = function(req, res) {
 
     //Get the current user id and the club they are joining
@@ -182,7 +182,7 @@ var joinClub = function(req, res) {
 
 }
 
-//DESCRIPTION OF FUNCTION
+//displays club page for admin
 var clubPageAdmin = function(req, res) {
 	adminId = req.params.adminid;
 	clubname = req.params.clubname;
@@ -202,6 +202,7 @@ var clubPageAdmin = function(req, res) {
 
 }
 
+//displays clubpage for general member
 var clubPage = function(req, res) {
     clubname = req.params.clubname;
 
@@ -220,17 +221,19 @@ var clubPage = function(req, res) {
 
 }
 
+//updates the club description
 var updateDescription = function(req, res) {
     console.log('inside update');
-    /*console.log(req.body);*/
+    console.log(req.body);
 
-    /*clubDb.changeClubDescription(req.body.clubName, req.body.welcomeBlurb, function(error){
+    clubDb.changeClubDescription(req.body.clubName, req.body.welcomeBlurb, function(error){
         if (error) {
             console.log('shit got fucked changing club description');
         } else {
             res.send('success');
         }    
-    });*/
+    });
+    res.redirect('/welcome');
 }
 
 var routes = {
