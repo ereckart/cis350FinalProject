4178b3d5fe3aec605cc82864c35e4c56474673c8
Daniella Erulker <derulker@gmail.com>
2018-04-18 21:40:26 -0400

    calendar on admin page

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,3 +1,6 @@
+//import $ from '../../../js/jquery.js';
+import '../../../js/fullcalendar.js';
+
 (function($) {
 
     //Display club name on admin screen
@@ -35,7 +38,7 @@
         $memberListForClubs.append('<li><input type="checkbox" id="memberSelectAll" id="selectall"> Select All </li>');
     }
     for (var i = 0; i < memberArray.length; i++) {
-        inputString = '<input type="checkbox" class="membercheckbox" id="member"'+ i + ' name="member' + i + '" value="' + memberArray[i] + '">' + memberArray[i];
+        var inputString = '<input type="checkbox" class="membercheckbox" id="member"'+ i + ' name="member' + i + '" value="' + memberArray[i] + '">' + memberArray[i];
         $memberListForClubs.append('<li>' + inputString +'</li>');
     }
 
@@ -61,5 +64,84 @@
                 " End Time: " + eventArray[i].endtime + '</li>');
         }
     }
+
+    //Show the calendar
+
+    $.getScript("../../../lib/moment.min.js");
+
+    $('#calendar').fullCalendar({
+      header: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'month,basicWeek,basicDay'
+      },
+      defaultDate: '2018-03-12',
+      navLinks: true, // can click day/week names to navigate views
+      editable: true,
+      eventLimit: true, // allow "more" link when too many events
+      events: [
+        {
+          title: 'All Day Event',
+          start: '2018-03-01'
+        },
+        {
+          title: 'Long Event',
+          start: '2018-03-07',
+          end: '2018-03-10'
+        },
+        {
+          id: 999,
+          title: 'Repeating Event',
+          start: '2018-03-09T16:00:00'
+        },
+        {
+          id: 999,
+          title: 'Repeating Event',
+          start: '2018-03-16T16:00:00'
+        },
+        {
+          title: 'Conference',
+          start: '2018-03-11',
+          end: '2018-03-13'
+        },
+        {
+          title: 'Meeting',
+          start: '2018-03-12T10:30:00',
+          end: '2018-03-12T12:30:00'
+        },
+        {
+          title: 'Lunch',
+          start: '2018-03-12T12:00:00'
+        },
+        {
+          title: 'Meeting',
+          start: '2018-03-12T14:30:00'
+        },
+        {
+          title: 'Happy Hour',
+          start: '2018-03-12T17:30:00'
+        },
+        {
+          title: 'Dinner',
+          start: '2018-03-12T20:00:00'
+        },
+        {
+          title: 'Birthday Party',
+          start: '2018-03-13T07:00:00'
+        },
+        {
+          title: 'Click for Google',
+          url: 'http://google.com/',
+          start: '2018-03-28'
+        }
+      ]
+    });
+
+var removeUndef = function() {
+    var original = $('.fc-center').find("h2").text();
+    $('.fc-center').find("h2").text(original.replace(new RegExp("undefined", 'g'), ""));
+};
+
+$(".fc-toolbar").on('click', '.fc-button', removeUndef);
 
 })(jQuery);
