e5d312f484da8f65e7018ea2151e32ec29fafdb3
Daniella Erulker <derulker@gmail.com>
2018-04-19 01:57:07 -0400

    added calendar id and api key

--- public/js/admin.js
+++ public/js/admin.js
@@ -66,8 +66,10 @@ import '../../../js/fullcalendar.js';
     }
 
     //Show the calendar
-
     $.getScript("../../../lib/moment.min.js");
+    $.getScript("../../../js/gcal.js");
+
+    console.log($.cookie('email'));
 
     $('#calendar').fullCalendar({
       header: {
@@ -75,66 +77,13 @@ import '../../../js/fullcalendar.js';
         center: 'title',
         right: 'month,basicWeek,basicDay'
       },
-      defaultDate: '2018-03-12',
-      navLinks: true, // can click day/week names to navigate views
-      editable: true,
-      eventLimit: true, // allow "more" link when too many events
-      events: [
-        {
-          title: 'All Day Event',
-          start: '2018-03-01'
-        },
-        {
-          title: 'Long Event',
-          start: '2018-03-07',
-          end: '2018-03-10'
-        },
-        {
-          id: 999,
-          title: 'Repeating Event',
-          start: '2018-03-09T16:00:00'
-        },
-        {
-          id: 999,
-          title: 'Repeating Event',
-          start: '2018-03-16T16:00:00'
-        },
-        {
-          title: 'Conference',
-          start: '2018-03-11',
-          end: '2018-03-13'
-        },
-        {
-          title: 'Meeting',
-          start: '2018-03-12T10:30:00',
-          end: '2018-03-12T12:30:00'
-        },
-        {
-          title: 'Lunch',
-          start: '2018-03-12T12:00:00'
-        },
-        {
-          title: 'Meeting',
-          start: '2018-03-12T14:30:00'
-        },
-        {
-          title: 'Happy Hour',
-          start: '2018-03-12T17:30:00'
-        },
-        {
-          title: 'Dinner',
-          start: '2018-03-12T20:00:00'
-        },
-        {
-          title: 'Birthday Party',
-          start: '2018-03-13T07:00:00'
-        },
-        {
-          title: 'Click for Google',
-          url: 'http://google.com/',
-          start: '2018-03-28'
-        }
-      ]
+      // defaultDate: '2018-03-12',
+      // navLinks: true, // can click day/week names to navigate views
+      // editable: true,
+      // eventLimit: true, // allow "more" link when too many events
+      googleCalendarApiKey: 'AIzaSyBeZyYpoe0d9CDaHALOu-aLsACKk2xVApk',
+       eventSources: [
+            { googleCalendarId: 'derulker@gmail.com'}]
     });
 
 var removeUndef = function() {
